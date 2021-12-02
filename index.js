033bec67c47ac1d273f411e305a1bd99f8829571
azum98 <78663264+azum98@users.noreply.github.com>
2021-12-02 15:22:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const yesBtn = document.querySelector('#yesBtn');
 
 yesBtn.addEventListener('click',function () {
-    alert('Siempre supe que aceptarias bb jaja');
+    //alert('Siempre supe que aceptarias bb jaja');
     alert('TE QUIERO UN MONTÓN 🧡');
 });
 
